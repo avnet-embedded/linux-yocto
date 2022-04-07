f9e94ae980b8dc1248f331f7c4cec4cb1c82c072
Ido Schimmel <idosch@nvidia.com>
2022-04-07 10:35:22 +0300

    net/sched: act_api: Add extack to offload_act_setup() callback
    commit c2ccf84ecb715bb81dc7f51e69d680a95bf055ae upstream
    
    The callback is used by various actions to populate the flow action
    structure prior to offload. Pass extack to this callback so that the
    various actions will be able to report accurate error messages to user
    space.
    
    Signed-off-by: Ido Schimmel <idosch@nvidia.com>
    Reviewed-by: Petr Machata <petrm@nvidia.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Rui He <rui.he@windriver.com>
    Reviewed-by: Zhaolong Zhang <zhaolong.zhang@windriver.com>

--- net/sched/act_police.c
+++ net/sched/act_police.c
@@ -420,7 +420,8 @@ static int tcf_police_search(struct net *net, struct tc_action **a, u32 index)
 }
 
 static int tcf_police_offload_act_setup(struct tc_action *act, void *entry_data,
-					u32 *index_inc, bool bind)
+					u32 *index_inc, bool bind,
+					struct netlink_ext_ack *extack)
 {
 	if (bind) {
 		struct flow_action_entry *entry = entry_data;
