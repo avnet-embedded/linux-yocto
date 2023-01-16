90724cf35bc10964e69d221ee8728026939299eb
Hariprasad Kelam <hkelam@marvell.com>
2023-01-16 22:10:53 +0530

    octeontx2-af: Reset MAC internal loopback in FLR
    commit 73b48bbf42526c5ca58c0fc51308de5a26c0a522 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    Upon receiving internal loopback feature request from netdev driver, AF
    driver configures this feature in the underlying MAC (CGX/RPM). This
    patch adds support to reset the same in FLR.
    
    Change-Id: I214280205b1b2d9e3ba3cb91e0ca56c9415e07f9
    Signed-off-by: Hariprasad Kelam <hkelam@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/94602
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Tested-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Signed-off-by: Li Wang <li.wang@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/cgx.h
+++ drivers/net/ethernet/marvell/octeontx2/af/cgx.h
@@ -203,7 +203,7 @@ int cgx_lmac_get_pfc_frm_cfg(void *cgxd, int lmac_id, u8 *tx_pause,
 			     u8 *rx_pause);
 int verify_lmac_fc_cfg(void *cgxd, int lmac_id, u8 tx_pause, u8 rx_pause,
 		       int pfvf_idx);
-int cgx_lmac_reset(void *cgxd, int lmac_id);
+int cgx_lmac_reset(void *cgxd, int lmac_id, u8 pf_req_flr);
 u8 cgx_lmac_get_p2x(int cgx_id, int lmac_id);
 u64 cgx_get_dmacflt_dropped_pktcnt(void *cgx, int lmac_id);
 
