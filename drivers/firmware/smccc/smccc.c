766080f63506efe23e548125a0176a8926a59e72
Rakesh Babu Saladi <rsaladi2@marvell.com>
2023-02-01 13:17:48 +0530

    arm64: smccc: Add trace events to SMC calls.
    commit f782ee03280814286283ac12ef23a3f7dab2bbce from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    This patch adds start and end trace events to an SMC call sent from
    kernel to ATF. The start trace event prints the smc_id and the end
    trace event prints the smc_id and the time taken to process the SMC
    call.
    
    Change-Id: If5175c1c47530bcfa7ce358a1b53b6cb70b70d16
    Signed-off-by: Rakesh Babu Saladi <rsaladi2@marvell.com>
    Signed-off-by: Tanmay Jagdale <tanmay@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/96755
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: George Cherian <gcherian@marvell.com>
    Signed-off-by: Li Wang <li.wang@windriver.com>

--- drivers/firmware/smccc/smccc.c
+++ drivers/firmware/smccc/smccc.c
@@ -7,6 +7,8 @@
 
 #include <linux/init.h>
 #include <linux/arm-smccc.h>
+#include <linux/sched/clock.h>
+#include "smccc_trace.h"
 
 static u32 smccc_version = ARM_SMCCC_VERSION_1_0;
 static enum arm_smccc_conduit smccc_conduit = SMCCC_CONDUIT_NONE;
@@ -31,3 +33,17 @@ u32 arm_smccc_get_version(void)
 	return smccc_version;
 }
 EXPORT_SYMBOL_GPL(arm_smccc_get_version);
+
+void arm_smccc_smc(unsigned long a0, unsigned long a1, unsigned long a2, unsigned long a3,
+		   unsigned long a4, unsigned long a5, unsigned long a6, unsigned long a7,
+		   struct arm_smccc_res *res)
+{
+	u64 start, elapsed;
+
+	trace_arm_smccc_smc_start(a0);
+	start = local_clock();
+	__arm_smccc_smc(a0, a1, a2, a3, a4, a5, a6, a7, res, NULL);
+	elapsed = local_clock() - start;
+	trace_arm_smccc_smc_end(a0, elapsed);
+}
+EXPORT_SYMBOL_GPL(arm_smccc_smc);
