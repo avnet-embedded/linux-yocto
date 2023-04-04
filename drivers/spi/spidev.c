bfaab2849f19cf7850e9a795472f8aa568297efa
Vishwaroop A <va@nvidia.com>
2023-04-04 12:04:31 +0000

    spi: add "tegra-spidev" compatible string.
    commit 391970d175268127f06eebf46b3246f68374c259 from
    https://nv-tegra.nvidia.com/r/3rdparty/canonical/linux-jammy.git jetson_36.3
    
    BugLink: https://bugs.launchpad.net/bugs/2019240
    
    Add "tegra-spidev" compatible string.
    
    Signed-off-by: Vishwaroop A <va@nvidia.com>
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
    Signed-off-by: Abhilash G <abhilashg@nvidia.com>
    Acked-by: Brad Figg <bfigg@nvidia.com>
    Acked-by: Jacob Martin <jacob.martin@canonical.com>
    Acked-by: Ian May <ian.may@canonical.com>
    Signed-off-by: Brad Figg <bfigg@nvidia.com>
    Signed-off-by: Rajeshkumar Ramasamy <rajeshkumar.ramasamy@windriver.com>
    Signed-off-by: Saravanan.K.S <saravanan.kadambathursubramaniyam@windriver.com>

--- drivers/spi/spidev.c
+++ drivers/spi/spidev.c
@@ -722,6 +722,8 @@ static const struct of_device_id spidev_dt_ids[] = {
 	{ .compatible = "rohm,dh2228fv", .data = &spidev_of_check },
 	{ .compatible = "semtech,sx1301", .data = &spidev_of_check },
 	{ .compatible = "silabs,em3581", .data = &spidev_of_check },
+        { .compatible = "nvidia,tegra-spidev" },
+        { .compatible = "tegra-spidev" },
 	{},
 };
 MODULE_DEVICE_TABLE(of, spidev_dt_ids);
