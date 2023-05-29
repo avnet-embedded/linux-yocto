5789f96feda6db06baa94945054bb92ddf273887
vinothkumarr <vinothkumarr@nvidia.com>
2023-05-29 12:40:27 +0530

    mtd: spi-nor: support for GD
    commit 6c48dcc7d59935297216ddf35d0db54ceafb91c9 from
    https://nv-tegra.nvidia.com/r/3rdparty/canonical/linux-jammy.git jetson_36.3
    
    BugLink: https://bugs.launchpad.net/bugs/2034622
    
    Add flash info for Gigadevice(GD25LT512M) device.
    
    http://nvbugs/3866554
    
    Signed-off-by: vinothkumarr <vinothkumarr@nvidia.com>
    Reviewed-by: Kartik Rajput <kkartik@nvidia.com>
    Tested-by: Kartik Rajput <kkartik@nvidia.com>
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Acked-by: Jacob Martin <jacob.martin@canonical.com>
    Acked-by: Noah Wager <noah.wager@canonical.com>
    Signed-off-by: Jacob Martin <jacob.martin@canonical.com>
    Signed-off-by: Rajeshkumar Ramasamy <rajeshkumar.ramasamy@windriver.com>
    Signed-off-by: Saravanan.K.S <saravanan.kadambathursubramaniyam@windriver.com>

--- drivers/mtd/spi-nor/gigadevice.c
+++ drivers/mtd/spi-nor/gigadevice.c
@@ -67,6 +67,11 @@ static const struct flash_info gigadevice_nor_parts[] = {
 		FLAGS(SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB | SPI_NOR_TB_SR_BIT6)
 		FIXUP_FLAGS(SPI_NOR_4B_OPCODES)
 		.fixups = &gd25q256_fixups },
+	{ "gd25lt512m", INFO(0xc8661a, 0, 64 * 1024, 1024)
+	        FLAGS(SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB | SPI_NOR_TB_SR_BIT6)
+	        FIXUP_FLAGS(SPI_NOR_4B_OPCODES)
+	        NO_SFDP_FLAGS(SECT_4K | SPI_NOR_QUAD_READ)
+		.fixups = &gd25q256_fixups },
 };
 
 const struct spi_nor_manufacturer spi_nor_gigadevice = {
