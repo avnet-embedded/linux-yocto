f03d3d50bb2d65dc0cb8f0bed8ac82573702e29e
Anshuman Khandual <anshuman.khandual@arm.com>
2023-08-17 09:29:26 +0530

    coresight: etm4x: Ensure valid drvdata and clock before clk_put()
    commit a4621fd1d4fd04fe2d8105a4d64e85cdc4a259b1 upstream
    
    This validates 'drvdata' and 'drvdata->pclk' clock before calling clk_put()
    in etm4_remove_platform_dev(). The problem was detected using Smatch static
    checker as reported.
    
    Fixes: 73d779a03a76a ("coresight: etm4x: Change etm4_platform_driver driver for MMIO devices")
    Cc: Suzuki K Poulose <suzuki.poulose@arm.com>
    Cc: Mike Leach <mike.leach@linaro.org>
    Cc: James Clark <james.clark@arm.com>
    Cc: coresight@lists.linaro.org
    Cc: linux-arm-kernel@lists.infradead.org
    Cc: linux-kernel@vger.kernel.org
    Reported-by: Dan Carpenter <dan.carpenter@linaro.org>
    Closes: https://lists.linaro.org/archives/list/coresight@lists.linaro.org/thread/G4N6P4OXELPLLQSNU3GU2MR4LOLRXRMJ/
    Reviewed-by: James Clark <james.clark@arm.com>
    Reviewed-by: Mike Leach <mike.leach@lnaro.org>
    Signed-off-by: Anshuman Khandual <anshuman.khandual@arm.com>
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Link: https://lore.kernel.org/r/20230817035926.157370-1-anshuman.khandual@arm.com
    Integrated-by: Jinchao Li <jinchao.li@windriver.com>

--- drivers/hwtracing/coresight/coresight-etm4x-core.c
+++ drivers/hwtracing/coresight/coresight-etm4x-core.c
@@ -2172,7 +2172,7 @@ static int etm4_remove_platform_dev(struct platform_device *pdev)
 		etm4_remove_dev(drvdata);
 	pm_runtime_disable(&pdev->dev);
 
-	if (drvdata->pclk)
+	if (drvdata && !IS_ERR_OR_NULL(drvdata->pclk))
 		clk_put(drvdata->pclk);
 
 	return 0;
