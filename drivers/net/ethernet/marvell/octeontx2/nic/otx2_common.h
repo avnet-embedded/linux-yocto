1e1e8985fe6759a442fa60fdc4f91e9fe63373a0
Geetha sowjanya <gakula@marvell.com>
2024-09-05 17:21:41 +0530

    octeontx2-pf: Refactoring RVU NIC driver
    commit dbd405acc3ec11066144be977e4b58bd3463b058 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    Refactoring and export list of shared functions such that
    they can be used by both RVU NIC and representor driver.
    
    Signed-off-by: Geetha sowjanya <gakula@marvell.com>
    Change-Id: Ib0a61662b8741140bb1444eeceb2034e4cc1d875
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/135112
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Signed-off-by: Bo Sun <Bo.Sun.CN@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_common.h
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_common.h
@@ -188,6 +188,7 @@ struct otx2_hw {
 	u16			txschq_list[NIX_TXSCH_LVL_CNT][MAX_TXSCHQ_PER_FUNC];
 	u16			matchall_ipolicer;
 	u32			dwrr_mtu;
+	u32			max_mtu;
 	u8			smq_link_type;
 
 	/* HW settings, coalescing etc */
@@ -1002,6 +1003,20 @@ int otx2_aura_init(struct otx2_nic *pfvf, int aura_id,
 		   int pool_id, int numptrs);
 void otx2_txschq_free_one(struct otx2_nic *pfvf, u16 lvl, u16 schq);
 void otx2_free_pending_sqe(struct otx2_nic *pfvf);
+int otx2_init_hw_resources(struct otx2_nic *pfvf);
+void otx2_free_hw_resources(struct otx2_nic *pf);
+int otx2_wq_init(struct otx2_nic *pf);
+int otx2_check_pf_usable(struct otx2_nic *pf);
+int otx2_pfaf_mbox_init(struct otx2_nic *pf);
+int otx2_register_mbox_intr(struct otx2_nic *pf, bool probe_af);
+int otx2_realloc_msix_vectors(struct otx2_nic *pf);
+void otx2_pfaf_mbox_destroy(struct otx2_nic *pf);
+void otx2_disable_mbox_intr(struct otx2_nic *pf);
+void otx2_free_queue_mem(struct otx2_qset *qset);
+int otx2_alloc_queue_mem(struct otx2_nic *pf);
+void otx2_disable_napi(struct otx2_nic *pf);
+irqreturn_t otx2_cq_intr_handler(int irq, void *cq_irq);
+int otx2_init_rsrc(struct pci_dev *pdev, struct otx2_nic *pf);
 
 /* RSS configuration APIs*/
 int otx2_rss_init(struct otx2_nic *pfvf);
