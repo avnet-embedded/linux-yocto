d0ed091689709cc808f7fb4ab70b905f255b6923
Bruce Ashfield <bruce.ashfield@gmail.com>
2025-07-11 11:21:20 -0400

    aufs6: standalone
    Signed-off-by: Bruce Ashfield <bruce.ashfield@gmail.com>

--- fs/namespace.c
+++ fs/namespace.c
@@ -1009,6 +1009,7 @@ int is_current_mnt_ns(struct vfsmount *mnt)
 {
 	return check_mnt(real_mount(mnt));
 }
+EXPORT_SYMBOL_GPL(is_current_mnt_ns);
 
 static inline bool check_anonymous_mnt(struct mount *mnt)
 {
@@ -2365,6 +2366,7 @@ struct path *collect_paths(const struct path *path,
 		path_get(p);
 	return res;
 }
+EXPORT_SYMBOL_GPL(collect_paths);
 
 void drop_collected_paths(struct path *paths, struct path *prealloc)
 {
@@ -2373,6 +2375,7 @@ void drop_collected_paths(struct path *paths, struct path *prealloc)
 	if (paths != prealloc)
 		kfree(paths);
 }
+EXPORT_SYMBOL_GPL(drop_collected_paths);
 
 static void free_mnt_ns(struct mnt_namespace *);
 static struct mnt_namespace *alloc_mnt_ns(struct user_namespace *, bool);
@@ -2461,6 +2464,7 @@ static bool __has_locked_children(struct mount *mnt, struct dentry *dentry)
 	}
 	return false;
 }
+EXPORT_SYMBOL_GPL(drop_collected_mounts);
 
 bool has_locked_children(struct mount *mnt, struct dentry *dentry)
 {
