5e6ddf32a80660b264f12ad7e5f3671407c67b91
David Yu <davyu@nvidia.com>
2026-01-04 09:40:40 +0800

    UBUNTU: SAUCE: net: phy: fix data corruption
    commit 0498ded7bc94eea3fc7d24bf90100ffdf1464529 from
    https://github.com/OE4T/linux-jammy-nvidia-tegra.git
    
    BugLink: https://bugs.launchpad.net/bugs/2019240
    
    Data corruption is happening in aqr107_config_init().
    It's accessing address offset across struct phy_device boundary.
    This is caused by misuse of SUPPORTED_* to linkmode_*_bit() api.
    Change SUPPORTED_* to ETHTOOL_LINK_MODE_*_BIT for correct usage.
    
    Signed-off-by: David Yu <davyu@nvidia.com>
    Reviewed-by: Narayan Reddy <narayanr@nvidia.com>
    Reviewed-by: Bhadram Varka <vbhadram@nvidia.com>
    Reviewed-by: Bibek Basu <bbasu@nvidia.com>
    Tested-by: Bhadram Varka <vbhadram@nvidia.com>
    Signed-off-by: Abhilash G <abhilashg@nvidia.com>
    Acked-by: Brad Figg <bfigg@nvidia.com>
    Acked-by: Jacob Martin <jacob.martin@canonical.com>
    Acked-by: Ian May <ian.may@canonical.com>
    Signed-off-by: Brad Figg <bfigg@nvidia.com>
    Signed-off-by: Meng Li <Meng.Li@windriver.com>
    Signed-off-by: Bruce Ashfield <bruce.ashfield@gmail.com>

--- drivers/net/phy/realtek.c
+++ drivers/net/phy/realtek.c
@@ -533,8 +533,8 @@ static int rtl8211f_config_init(struct phy_device *phydev)
 	}
 
 	/* Advertise Flow Control */
-	linkmode_set_bit(SUPPORTED_Pause, phydev->supported);
-	linkmode_set_bit(SUPPORTED_Asym_Pause, phydev->supported);
+	linkmode_set_bit(ETHTOOL_LINK_MODE_Pause_BIT, phydev->supported);
+	linkmode_set_bit(ETHTOOL_LINK_MODE_Asym_Pause_BIT, phydev->supported);
 	linkmode_copy(phydev->advertising, phydev->supported);
 	
 	if (priv->has_phycr2) {
