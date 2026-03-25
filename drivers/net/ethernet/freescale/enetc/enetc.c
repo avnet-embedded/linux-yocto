boundarydevices/linux

LF-13647 net: enetc: add EEE support for enetc4
Add EEE support for i.MX95/ENETC V4. It will be disabled by default.
Can enable it with this command:
$ ethtool --set-eee eth0 tx-timer 5000 tx-lpi on eee on

Signed-off-by: Clark Wang <xiaoning.wang@nxp.com>
Reviewed-by: Wei Fang <wei.fang@nxp.com>

drivers/net/ethernet/freescale/enetc/enetc.c
@@ -2974,8 +2974,8 @@ static void enetc_clear_interrupts(struct enetc_ndev_priv *priv)
 static int enetc_phylink_connect(struct net_device *ndev)
 {
 	struct enetc_ndev_priv *priv = netdev_priv(ndev);
+	struct ethtool_eee *edata = &priv->eee;
 	struct enetc_si *si = priv->si;
-	struct ethtool_eee edata;
 	int err;
 
 	if (!priv->phylink) {
@@ -3001,9 +3001,8 @@ static int enetc_phylink_connect(struct net_device *ndev)
 		return err;
 	}
 
-	/* disable EEE autoneg, until ENETC driver supports it */
-	memset(&edata, 0, sizeof(struct ethtool_eee));
-	phylink_ethtool_set_eee(priv->phylink, &edata);
+	/* disable EEE autoneg when boot, can enable it by ethtool */
+	phylink_ethtool_set_eee(priv->phylink, edata);
 
 	phylink_start(priv->phylink);
 
@@ -3058,6 +3057,20 @@ void enetc_start(struct net_device *ndev)
 }
 EXPORT_SYMBOL_GPL(enetc_start);
 
+static void enetc_set_eee(struct net_device *ndev)
+{
+	struct enetc_ndev_priv *priv = netdev_priv(ndev);
+	struct ethtool_eee *eee = &priv->eee;
+
+	if (is_enetc_rev1(priv->si) || !enetc_si_is_pf(priv->si))
+		return;
+
+	if (eee->eee_enabled && eee->tx_lpi_enabled && eee->tx_lpi_timer)
+		enetc_eee_mode_set(ndev, true);
+	else
+		enetc_eee_mode_set(ndev, false);
+}
+
 int enetc_open(struct net_device *ndev)
 {
 	struct enetc_ndev_priv *priv = netdev_priv(ndev);
@@ -3098,6 +3111,8 @@ int enetc_open(struct net_device *ndev)
 	enetc_assign_rx_resources(priv, rx_res);
 	enetc_setup_bdrs(priv, extended);
 	enetc_start(ndev);
+
+	enetc_set_eee(ndev);
 
 	return 0;
 
@@ -3338,6 +3353,8 @@ int enetc_resume(struct net_device *ndev, bool wol)
 	enetc_assign_rx_resources(priv, rx_res);
 	enetc_setup_bdrs(priv, extended);
 	enetc_start(priv->ndev);
+
+	enetc_set_eee(ndev);
 
 	return 0;
 

drivers/net/ethernet/freescale/enetc/enetc.h
@@ -502,6 +502,8 @@ struct enetc_ndev_priv {
 	int max_ipf_entries;
 	u32 ipt_wol_eid;
 
+	struct ethtool_eee eee;
+
 	union psfp_cap psfp_cap;
 	struct enetc_psfp_chain psfp_chain;
 	unsigned long *ist_bitmap;
@@ -579,6 +581,7 @@ void enetc_refresh_vlan_ht_filter(struct enetc_si *si);
 void enetc_set_ethtool_ops(struct net_device *ndev);
 void enetc_mm_link_state_update(struct enetc_ndev_priv *priv, bool link);
 void enetc_mm_commit_preemptible_tcs(struct enetc_ndev_priv *priv);
+void enetc_eee_mode_set(struct net_device *dev, bool enable);
 
 /* control buffer descriptor ring (CBDR) */
 int enetc_init_cbdr(struct enetc_si *si);

drivers/net/ethernet/freescale/enetc/enetc4_hw.h
@@ -333,6 +333,10 @@
 /* Port MAC 0/1 Pause Quanta Threshold Register */
 #define ENETC4_PM_PAUSE_THRESH(mac)	(0x5064 + (mac) * 0x400)
 
+#define ENETC4_PM_LPWAKE_TIMER(mac)	(0x50B8 + (mac) * 0x400)
+#define ENETC4_PM_SLEEP_TIMER(mac)	(0x50BC + (mac) * 0x400)
+#define  PM_EEE_TIMER			GENMASK(23, 0)
+
 #define ENETC4_PM_SINGLE_STEP(mac)	(0x50C0 + (mac) * 0x400)
 #define  PM_SINGLE_STEP_CH		BIT(6)
 #define  PM_SINGLE_STEP_OFFSET_MASK	GENMASK(15, 7)

avnet-embedded/linux-yocto

commit  14382c4113c24d780366d4c0e3a619dd53e596cc from
https://github.com/nxp-imx/linux-imx lf-6.18.y
(cherry picked from commit 7b53effc703fee23e640eda1c471c324b82675a4)
Signed-off-by: Xiaolei Wang <xiaolei.wang@windriver.com>

drivers/net/ethernet/freescale/enetc/enetc.c
@@ -2955,8 +2955,8 @@ static void enetc_clear_interrupts(struct enetc_ndev_priv *priv)
 static int enetc_phylink_connect(struct net_device *ndev)
 {
 	struct enetc_ndev_priv *priv = netdev_priv(ndev);
+	struct ethtool_keee *edata = &priv->eee;
 	struct enetc_si *si = priv->si;
-	struct ethtool_keee edata;
 	int err;
 
 	if (!priv->phylink) {
@@ -2981,9 +2981,8 @@ static int enetc_phylink_connect(struct net_device *ndev)
 		return err;
 	}
 
-	/* disable EEE autoneg, until ENETC driver supports it */
-	memset(&edata, 0, sizeof(struct ethtool_keee));
-	phylink_ethtool_set_eee(priv->phylink, &edata);
+	/* disable EEE autoneg when boot, can enable it by ethtool */
+	phylink_ethtool_set_eee(priv->phylink, edata);
 
 	phylink_start(priv->phylink);
 
@@ -3038,6 +3037,20 @@ void enetc_start(struct net_device *ndev)
 }
 EXPORT_SYMBOL_GPL(enetc_start);
 
+static void enetc_set_eee(struct net_device *ndev)
+{
+	struct enetc_ndev_priv *priv = netdev_priv(ndev);
+	struct ethtool_keee *eee = &priv->eee;
+
+	if (is_enetc_rev1(priv->si) || !enetc_si_is_pf(priv->si))
+		return;
+
+	if (eee->eee_enabled && eee->tx_lpi_enabled && eee->tx_lpi_timer)
+		enetc_eee_mode_set(ndev, true);
+	else
+		enetc_eee_mode_set(ndev, false);
+}
+
 int enetc_open(struct net_device *ndev)
 {
 	struct enetc_ndev_priv *priv = netdev_priv(ndev);
@@ -3078,6 +3091,8 @@ int enetc_open(struct net_device *ndev)
 	enetc_assign_rx_resources(priv, rx_res);
 	enetc_setup_bdrs(priv, extended);
 	enetc_start(ndev);
+
+	enetc_set_eee(ndev);
 
 	return 0;
 
@@ -3318,6 +3333,8 @@ int enetc_resume(struct net_device *ndev, bool wol)
 	enetc_assign_rx_resources(priv, rx_res);
 	enetc_setup_bdrs(priv, extended);
 	enetc_start(priv->ndev);
+
+	enetc_set_eee(ndev);
 
 	return 0;
 

drivers/net/ethernet/freescale/enetc/enetc.h
@@ -520,6 +520,8 @@ struct enetc_ndev_priv {
 	struct pci_dev *rcec;
 	u32 ipt_wol_eid;
 	int wolopts;
+
+	struct ethtool_keee eee;
 };
 
 #define ENETC_CBD(R, i)	(&(((struct enetc_cbd *)((R).bd_base))[i]))
@@ -575,6 +577,7 @@ extern const struct ethtool_ops enetc4_pf_ethtool_ops;
 extern const struct ethtool_ops enetc_vf_ethtool_ops;
 void enetc_set_ethtool_ops(struct net_device *ndev);
 void enetc_mm_commit_preemptible_tcs(struct enetc_ndev_priv *priv);
+void enetc_eee_mode_set(struct net_device *dev, bool enable);
 
 /* control buffer descriptor ring (CBDR) */
 int enetc_setup_cbdr(struct enetc_si *si);

drivers/net/ethernet/freescale/enetc/enetc4_hw.h
@@ -293,6 +293,10 @@
 /* Port MAC 0/1 Pause Quanta Threshold Register */
 #define ENETC4_PM_PAUSE_THRESH(mac)	(0x5064 + (mac) * 0x400)
 
+#define ENETC4_PM_LPWAKE_TIMER(mac)	(0x50b8 + (mac) * 0x400)
+#define ENETC4_PM_SLEEP_TIMER(mac)	(0x50bc + (mac) * 0x400)
+#define  PM_EEE_TIMER			GENMASK(23, 0)
+
 #define ENETC4_PM_SINGLE_STEP(mac)	(0x50c0 + (mac) * 0x400)
 #define  PM_SINGLE_STEP_CH		BIT(6)
 #define  PM_SINGLE_STEP_OFFSET		GENMASK(15, 7)