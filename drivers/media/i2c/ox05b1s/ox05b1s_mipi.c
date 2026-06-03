ba6706f596e44f02ef05d437fa561586d07757a3
Xiaolei Wang <xiaolei.wang@windriver.com>
2026-06-03 12:53:46 +0800

    media: ox05b1s: Use v4l2_ctrl_handler_setup instead of __v4l2_ctrl_handler_setup
    The __v4l2_ctrl_handler_setup() function contains an assertion to verify
    that the v4l2_ctrl_handler::lock is held, as it should only be called when
    the lock has already been acquired. However, in ox05b1s_apply_current_mode(),
    __v4l2_ctrl_handler_setup() is called without first acquiring the lock.
    Fixes the issue by using v4l2_ctrl_handler_setup() instead.
    
    Fixes: f229a054d39c ("LF-13436: media: ox05b1s: Fix hdr_mode control")
    Signed-off-by: Xiaolei Wang <xiaolei.wang@windriver.com>
    Signed-off-by: Bruce Ashfield <bruce.ashfield@gmail.com>

--- drivers/media/i2c/ox05b1s/ox05b1s_mipi.c
+++ drivers/media/i2c/ox05b1s/ox05b1s_mipi.c
@@ -1217,7 +1217,7 @@ static int ox05b1s_apply_current_mode(struct ox05b1s *sensor)
 		goto out;
 
 	/* setup handler will write actual controls into sensor registers */
-	ret = __v4l2_ctrl_handler_setup(&sensor->ctrls.handler);
+	ret =  v4l2_ctrl_handler_setup(&sensor->ctrls.handler);
 
 out:
 	if (ret < 0)
